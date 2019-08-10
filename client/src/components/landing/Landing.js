0a6433c39cae1b6cda2689492d5fcb8052c7c008
Tarald Johansen <tarald.johansen@outlook.com>
2019-08-10 02:56:44 +0200

    Created landingpage component

---
+++ client/src/components/landing/Landing.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Landing = () => {
+    return (
+        <div>
+            <h1>hello</h1>
+            <h1>hello</h1>
+            <h1>hello</h1>
+            <h1>hello</h1>
+            <h1>hello</h1>
+            <h1>hello</h1>
+            <h1>hello</h1>
+            <h1>hello</h1>
+            <h1>hello</h1>
+            <h1>hello</h1>
+            <h1>hello</h1>
+            <h1>hello</h1>
+            <h1>hello</h1>
+            <h1>hello</h1>
+            <h1>hello</h1>
+            <h1>hello</h1>
+            <h1>hello</h1>
+            <h1>hello</h1>
+            <h1>hello</h1>
+            <h1>hello</h1>
+            <h1>hello</h1>
+        </div>
+    );
+};
+
+export default Landing;
