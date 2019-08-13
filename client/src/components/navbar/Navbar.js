07b2a878605a1ae19371b368c4641a89c2b45826
Tarald Johansen <tarald.johansen@outlook.com>
2019-08-13 16:44:24 +0200

    Refactored navbar

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -2,49 +2,9 @@ import React from "react";
 import styled from "styled-components";
 import { withRouter } from "react-router-dom";
 import Logo from "../../images/Logo.png";
+import RenderNavLinks from "../../lib/RenderNavLinks";
 
-const options = [
-    {
-        path: "/",
-        text: "Hjem"
-    },
-    {
-        path: "/about",
-        text: "Om Oss"
-    },
-    {
-        path: "/excercise",
-        text: "Trening"
-    },
-    {
-        path: "/physio",
-        text: "Fysio"
-    },
-    {
-        path: "/company",
-        text: "Bedrift"
-    }
-];
-
-const Navbar = ({ match, history }) => {
-    const activePath = match.url;
-
-    const renderOptions = () => {
-        return options.map((option, i) => {
-            return (
-                <StyledLi
-                    key={`nav-option-${i}`}
-                    className={option.path === activePath ? "current" : null}
-                    onClick={() => {
-                        history.push(`${option.path}`);
-                    }}
-                >
-                    {option.text}
-                </StyledLi>
-            );
-        });
-    };
-
+const Navbar = ({ history }) => {
     return (
         <StyledNav>
             <StyledContainer>
@@ -54,7 +14,7 @@ const Navbar = ({ match, history }) => {
                         history.push("/");
                     }}
                 />
-                <StyledUl>{renderOptions()}</StyledUl>
+                <RenderNavLinks />
             </StyledContainer>
         </StyledNav>
     );
@@ -72,31 +32,16 @@ const StyledContainer = styled.div`
     align-items: center;
     justify-content: space-between;
     display: flex;
+
+    @media screen and (max-width: 850px) {
+        flex-direction: column;
+        margin: 0 2rem;
+    }
 `;
 
 const StyledLogo = styled.img`
     height: 100px;
     :hover {
-        cursor: pointer;
-    }
-`;
-
-const StyledUl = styled.ul`
-    list-style: none;
-    display: flex;
-    padding: 0;
-    margin: 0;
-    .current {
-        color: #fc43d4;
-    }
-`;
-
-const StyledLi = styled.li`
-    color: #7bdecb;
-    padding: 1rem;
-    transition: 0.2s all ease-in-out;
-    :hover {
-        color: #fc43d4;
         cursor: pointer;
     }
 `;
