5162f1df544ca03a4a6f3c38e956ab4de40574a6
Tarald Johansen <tarald.johansen@outlook.com>
2019-08-13 21:28:58 +0200

    Added carousel to landingpage

--- client/src/components/landing/Landing.js
+++ client/src/components/landing/Landing.js
@@ -1,17 +1,11 @@
 import React from "react";
-import ImageCarousel from "../carousel/Carousel";
+import LandingCarousel from "../carousel/Carousel";
 import carousel_1 from "../../images/carousel_1.png";
 
 const Landing = () => {
-    const carouselItems = (
-        <div>
-            <img src={carousel_1} />
-        </div>
-    );
-
     return (
         <div>
-            <ImageCarousel {...carouselItems} />
+            <LandingCarousel />
             <h1>hello</h1>
             <h1>hello</h1>
             <h1>hello</h1>
