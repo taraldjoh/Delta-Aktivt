007a8c818d52903864d390633216fd3110c71e73
Tarald Johansen <tarald.johansen@outlook.com>
2019-08-14 19:24:22 +0200

    Added pageload fadein effect

--- client/src/App.js
+++ client/src/App.js
@@ -18,6 +18,11 @@ const GlobalStyle = createGlobalStyle`
         margin: 0;
         padding: 0;
         font-family: 'Oswald', sans-serif;
+        -webkit-animation: fadein 2s; /* Safari, Chrome and Opera > 12.1 */
+       -moz-animation: fadein 2s; /* Firefox < 16 */
+        -ms-animation: fadein 2s; /* Internet Explorer */
+         -o-animation: fadein 2s; /* Opera < 12.1 */
+            animation: fadein 2s;
     }
 
     h1, h2, h3, h4, h5, a {
@@ -61,4 +66,33 @@ const GlobalStyle = createGlobalStyle`
         user-select: none; /* Non-prefixed version, currently
                                   supported by Chrome and Opera */
     }
+
+    @keyframes fadein {
+    from { opacity: 0; }
+    to   { opacity: 1; }
+}
+
+/* Firefox < 16 */
+@-moz-keyframes fadein {
+    from { opacity: 0; }
+    to   { opacity: 1; }
+}
+
+/* Safari, Chrome and Opera > 12.1 */
+@-webkit-keyframes fadein {
+    from { opacity: 0; }
+    to   { opacity: 1; }
+}
+
+/* Internet Explorer */
+@-ms-keyframes fadein {
+    from { opacity: 0; }
+    to   { opacity: 1; }
+}
+
+/* Opera < 12.1 */
+@-o-keyframes fadein {
+    from { opacity: 0; }
+    to   { opacity: 1; }
+}
 `;
