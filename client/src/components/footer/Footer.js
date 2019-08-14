415e9016233609277d5fd55e5bfcd42fd9bcde28
Tarald Johansen <tarald.johansen@outlook.com>
2019-08-14 18:47:42 +0200

    Completed Footer component

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -1,39 +1,59 @@
-import React from "react";
+import React, { Fragment } from "react";
 import styled from "styled-components";
+import { withRouter } from "react-router-dom";
 
-const footerContent = [
-    {
-        content: (
-            <ul>
-                <li>Kråmyrvegen 3, 6007 Ålesund</li>
-                <li>Org.nr. 911 574 861</li>
-                <li>Telefon: 959 00 212</li>
-                <li>
-                    Mail:{" "}
-                    <a href="mailto:ingrid@deltaaktivt.no">
-                        ingrid@deltaaktivt.no
-                    </a>
-                </li>
-            </ul>
-        )
-    },
-    {
-        content: (
-            <ul>
-                <li>Hello</li>
-            </ul>
-        )
-    },
-    {
-        content: (
-            <p>
-                DELTA AKTIVT 2019 <br />© ALL RIGHTS RESERVED
-            </p>
-        )
-    }
-];
+import { Facebook, Instagram } from "react-feather";
 
-const Footer = () => {
+const Footer = ({ history }) => {
+    const footerContent = [
+        {
+            content: (
+                <StyledUl className={"sectionOne"}>
+                    <li>Kråmyrvegen 3, 6007 Ålesund</li>
+                    <li>Org.nr. 911 574 861</li>
+                    <li>Telefon: 959 00 212</li>
+                    <li>Mail: ingrid@deltaaktivt.no</li>
+                </StyledUl>
+            )
+        },
+        {
+            content: (
+                <Fragment>
+                    <div className={"sectionTwo"}>
+                        <StyledUl>
+                            <li>
+                                <a
+                                    rel="noopener noreferrer"
+                                    target="_blank"
+                                    href="https://www.facebook.com"
+                                >
+                                    <Facebook size={50} />
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    rel="noopener noreferrer"
+                                    target="_blank"
+                                    href="https://www.instagram.com"
+                                >
+                                    <Instagram size={50} />
+                                </a>
+                            </li>
+                        </StyledUl>
+                        <p>@deltaaktivt</p>
+                    </div>
+                </Fragment>
+            )
+        },
+        {
+            content: (
+                <StyledUl className={"sectionThree"}>
+                    <li>DELTA AKTIVT 2019</li>
+                    <li>ALL RIGHTS RESERVED</li>
+                </StyledUl>
+            )
+        }
+    ];
     const renderFooterItems = () => {
         return footerContent.map((item, i) => {
             return (
@@ -45,23 +65,66 @@ const Footer = () => {
     };
 
     return (
-        <StyledFooter>
+        <StyledFooter className={"noSelect"}>
             <StyledFooterContainer>{renderFooterItems()}</StyledFooterContainer>
         </StyledFooter>
     );
 };
 
-export default Footer;
+export default withRouter(Footer);
 
 const StyledFooter = styled.footer`
     background-color: #7bdecb;
     color: #fff;
-    padding: 0 5rem;
+    height: 150px;
 `;
 
 const StyledFooterContainer = styled.div`
     display: flex;
     justify-content: space-between;
+    margin: 0 2rem;
+    padding: 0 1rem;
+    height: 100%;
 `;
 
-const StyledSection = styled.div``;
+const StyledSection = styled.div`
+    height: 100%;
+    display: flex;
+    align-items: center;
+
+    .sectionOne {
+        flex-direction: column;
+    }
+    .sectionTwo {
+        li {
+            margin: 0 0.75rem;
+        }
+
+        a {
+            color: #fff;
+        }
+
+        p {
+            margin-top: 0.25rem;
+            color: #fff;
+            text-align: center;
+        }
+
+        svg {
+            transition: all 0.2s ease-in-out;
+        }
+        svg:hover {
+            cursor: pointer;
+            color: #f98fa0;
+        }
+    }
+    .sectionThree {
+        flex-direction: column;
+        text-align: right;
+    }
+`;
+
+const StyledUl = styled.ul`
+    margin: 0;
+    display: flex;
+`;
