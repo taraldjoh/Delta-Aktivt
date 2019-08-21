a284e61a6429eec8074dab005af2f5a0d94e7068
Tarald Johansen <tarald.johansen@outlook.com>
2019-08-21 20:05:49 +0200

    optimized navbar, added conditional navbar based on screen size

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,10 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { withRouter } from "react-router-dom";
 import Logo from "../../images/Logo.png";
-import RenderNavLinks from "../../lib/RenderNavLinks";
+import RenderNavLinks from "./RenderNavLinks";
+import NavMobile from "./NavMobile";
+
+const options = [
+    {
+        path: "/",
+        text: "Hjem"
+    },
+    {
+        path: "/about",
+        text: "Om Oss"
+    },
+    {
+        path: "/exercise",
+        text: "Trening"
+    },
+    {
+        path: "/services",
+        text: "Tjenester"
+    },
+    {
+        path: "/activities",
+        text: "Aktiviteter"
+    }
+];
 
 const Navbar = ({ history }) => {
+    const limit = 1000;
+
+    const [currWidth, setCurrWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        window.addEventListener("resize", updateWidth);
+        return () => {
+            window.removeEventListener("resize", updateWidth);
+        };
+    }, []);
+
+    const updateWidth = () => {
+        setCurrWidth(window.innerWidth);
+    };
+
+    const renderOptions = () => {
+        return options.map((option, i) => {
+            return (
+                <StyledLi onClick={() => history.push(`${option.path}`)}>
+                    {option.text}
+                </StyledLi>
+            );
+        });
+    };
+
     return (
         <StyledNav className="noSelect">
             <StyledContainer>
@@ -15,7 +64,11 @@ const Navbar = ({ history }) => {
                     }}
                     alt="brand-logo"
                 />
-                <RenderNavLinks />
+                {currWidth <= limit ? (
+                    <NavMobile />
+                ) : (
+                    <StyledUl>{renderOptions()}</StyledUl>
+                )}
             </StyledContainer>
         </StyledNav>
     );
@@ -45,5 +98,27 @@ const StyledLogo = styled.img`
     height: 100px;
     :hover {
         cursor: pointer;
+    }
+`;
+
+const StyledLi = styled.li`
+    color: #158175;
+    padding: 1rem;
+    font-size: 1.25rem;
+    letter-spacing: 1.25;
+    transition: 0.2s all ease-in-out;
+    :hover {
+        color: #d500b0;
+        cursor: pointer;
+    }
+`;
+
+const StyledUl = styled.ul`
+    list-style: none;
+    display: flex;
+    padding: 0;
+    margin: 0;
+    .current {
+        color: #d500b0;
     }
 `;
