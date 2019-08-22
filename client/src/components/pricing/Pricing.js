1b1573a8f99eeb76758982fd93ca6251c56189a1
Tarald Johansen <tarald.johansen@outlook.com>
2019-08-22 20:42:03 +0200

    Moved priced items to one file

--- client/src/components/pricing/Pricing.js
+++ client/src/components/pricing/Pricing.js
@@ -1,7 +1,44 @@
 import React from "react";
 import styled from "styled-components";
 
-import pricedProducts from "./pricedProducts";
+const pricedProducts = [
+    {
+        name: "Førstegangskonsultasjon (m/undersøkelse)",
+        price: "550,-"
+    },
+    {
+        name: "Fysikalsk behandling inntil 45 min",
+        price: "400,-"
+    },
+    {
+        name: "Massasje 30 min",
+        price: "350,-"
+    },
+    {
+        name: "Massasje 60 min",
+        price: "700,-"
+    },
+    {
+        name: "Sjokkbølgebehandling",
+        price: "500,-"
+    },
+    {
+        name: "Personlig trening førstegangskonsultasjon",
+        price: "750,-"
+    },
+    {
+        name: "Personlig trening 60 min",
+        price: "550,-"
+    },
+    {
+        name: "Maxpulstest",
+        price: "500,-"
+    },
+    {
+        name: "Fotanalyse og individuell tilpasning av Formthotics såler",
+        price: "1350,-"
+    }
+];
 
 const extraItems = [
     {
